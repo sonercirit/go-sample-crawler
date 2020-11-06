c41d32b2b150336c365ea44ce38d2b9ce92d02ca
sonercirit <sonercirit@gmail.com>
2020-11-06 21:24:45 +0300

    refactor into methods

--- crawler.go
+++ crawler.go
@@ -18,6 +18,11 @@ type book struct {
 	numberOfRatings int
 }
 
+var regexes struct {
+	averageRatingRegex   *regexp.Regexp
+	numberOfRatingsRegex *regexp.Regexp
+}
+
 func getInput(question string, def string) string {
 	// read from stdin
 	reader := bufio.NewReader(os.Stdin)
@@ -39,12 +44,95 @@ func getInput(question string, def string) string {
 	return text
 }
 
+func compileRegexes() {
+	// average rating regex
+	regexes.averageRatingRegex = regexp.MustCompile("([\\d|.]*) avg rating")
+	// number of ratings regex
+	regexes.numberOfRatingsRegex = regexp.MustCompile("([\\d|,]*) ratings")
+}
+
+func getDetails(e *colly.HTMLElement) (float64, int) {
+	// get text
+	text := e.ChildText(".uitext.greyText.smallText")
+
+	// assign average rating
+	averageRating := regexes.averageRatingRegex.FindStringSubmatch(text)[1]
+	// convert to float
+	averageRatingFloat, err := strconv.ParseFloat(averageRating, 32)
+	if err != nil {
+		log.Fatal("error while parsing averageRating to int: ", err)
+	}
+
+	// assign number of ratings
+	numberOfRatings := regexes.numberOfRatingsRegex.FindStringSubmatch(text)[1]
+	// remove commas from number of ratings
+	numberOfRatings = strings.ReplaceAll(numberOfRatings, ",", "")
+	// convert to integer
+	numberOfRatingsInt, err := strconv.Atoi(numberOfRatings)
+	if err != nil {
+		log.Fatal("error while parsing numberOfRatings to int: ", err)
+	}
+
+	// return the final results
+	return averageRatingFloat, numberOfRatingsInt
+}
+
+func getAuthors(e *colly.HTMLElement) []string {
+	// create the authors array
+	var authors []string
+	// for each author
+	e.ForEach(".authorName__container", func(i int, e *colly.HTMLElement) {
+		// add the author to array
+		authors = append(authors, strings.TrimSpace(e.Text))
+	})
+	// return the final results
+	return authors
+}
+
+func handleBooks(c *colly.Collector, books []book, bookCount int) {
+	// for each book result
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		// increment the counter for each book
+		bookCount++
+		// get the book name
+		name := e.ChildText(".bookTitle")
+		// log the founded book
+		log.Println("Found book:", name)
+
+		// get authors array
+		authors := getAuthors(e)
+
+		// parse and get details string
+		averageRatingFloat, numberOfRatingsInt := getDetails(e)
+
+		// generate and add struct to books array
+		books = append(books, book{
+			title:           name,
+			authors:         authors,
+			averageRating:   float32(averageRatingFloat),
+			numberOfRatings: numberOfRatingsInt,
+		})
+	})
+}
+
+func startScraping(pageCountInt int, query string, c *colly.Collector) {
+	// for every page - i starts at 1 and goes till including pageCountInt
+	for i := 1; i <= pageCountInt; i++ {
+		// generate url
+		url := fmt.Sprintf("https://www.goodreads.com/search?page=%d&q=%s", i, query)
+		// print url that is going to be parsed
+		log.Println("Going to parse:", url)
+		// start scraping
+		err := c.Visit(url)
+		if err != nil {
+			log.Fatal("error while doing the request: ", err)
+		}
+	}
+}
+
 func main() {
 	// compile regexes
-	// average rating regex
-	averageRatingRegex := regexp.MustCompile("([\\d|.]*) avg rating")
-	// number of ratings regex
-	numberOfRatingsRegex := regexp.MustCompile("([\\d|,]*) ratings")
+	compileRegexes()
 
 	// ask about the query
 	query := getInput("What should we search for?", "fantasy")
@@ -65,65 +153,11 @@ func main() {
 	var books []book
 	// start the book counter
 	bookCount := 0
-	// for each book result
-	c.OnHTML("tr", func(e *colly.HTMLElement) {
-		// increment the counter for each book
-		bookCount++
-		// get the book name
-		name := e.ChildText(".bookTitle")
-		// log the founded book
-		log.Println("Found book:", name)
+	// register book handler
+	handleBooks(c, books, bookCount)
 
-		// create the authors array
-		var authors []string
-		// for each author
-		e.ForEach(".authorName__container", func(i int, e *colly.HTMLElement) {
-			// add the author to array
-			authors = append(authors, strings.TrimSpace(e.Text))
-		})
-
-		// get text
-		text := e.ChildText(".uitext.greyText.smallText")
-
-		// assign average rating
-		averageRating := averageRatingRegex.FindStringSubmatch(text)[1]
-		// convert to float
-		averageRatingFloat, err := strconv.ParseFloat(averageRating, 32)
-		if err != nil {
-			log.Fatal("error while parsing averageRating to int: ", err)
-		}
-
-		// assign number of ratings
-		numberOfRatings := numberOfRatingsRegex.FindStringSubmatch(text)[1]
-		// remove commas from number of ratings
-		numberOfRatings = strings.ReplaceAll(numberOfRatings, ",", "")
-		// convert to integer
-		numberOfRatingsInt, err := strconv.Atoi(numberOfRatings)
-		if err != nil {
-			log.Fatal("error while parsing numberOfRatings to int: ", err)
-		}
-
-		// generate and add struct to books array
-		books = append(books, book{
-			title:           name,
-			authors:         authors,
-			averageRating:   float32(averageRatingFloat),
-			numberOfRatings: numberOfRatingsInt,
-		})
-	})
-
-	// for every page - i starts at 1 and goes till including pageCountInt
-	for i := 1; i <= pageCountInt; i++ {
-		// generate url
-		url := fmt.Sprintf("https://www.goodreads.com/search?page=%d&q=%s", i, query)
-		// print url that is going to be parsed
-		log.Println("Going to parse:", url)
-		// start scraping
-		err = c.Visit(url)
-		if err != nil {
-			log.Fatal("error while doing the request: ", err)
-		}
-	}
+	// start scraping
+	startScraping(pageCountInt, query, c)
 
 	// wait for all the threads to finish
 	c.Wait()
