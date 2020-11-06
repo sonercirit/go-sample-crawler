9f0ff5614530b2545baf898e0d840bd64f62b4ac
sonercirit <sonercirit@gmail.com>
2020-11-06 18:17:53 +0300

    add default value support for inputs

--- crawler.go
+++ crawler.go
@@ -8,18 +8,22 @@ import (
 	"strings"
 )
 
-func getInput(question string) string {
+func getInput(question string, def string) string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(question + ": ")
+	fmt.Printf("%s Default %s: ", question, def)
 	text, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal("can't read input: ", err)
 	}
-	return strings.TrimSpace(text)
+	text = strings.TrimSpace(text)
+	if text == "" {
+		text = def
+	}
+	return text
 }
 
 func main() {
-	query := getInput("What should we search for? For example \"business\"")
-	pageCount := getInput("How many pages should we crawl?")
+	query := getInput("What should we search for?", "fantasy")
+	pageCount := getInput("How many pages should we crawl?", "10")
 	log.Println(query, pageCount)
 }
